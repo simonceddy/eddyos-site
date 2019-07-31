8e799b7cdda25e5cbb83066d535d3804c1b94d84
Simon Eddy <simoneddy@Simons-MacBook-Pro.local>
2019-07-31 12:38:03 +1000

    moved styles from old src, added stylelintignore

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+  <div>
+    Test
+  </div>,
+  document.getElementById('root')
+);
+
+// console.log(store);
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();

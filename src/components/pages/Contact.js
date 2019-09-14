9d8adca6d74ad4cf51614ac980396083bab7ebe5
Simon Eddy <simon@simoneddy.com.au>
2019-09-14 18:13:26 +1000

    split test article to own file

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import Text from '../Text';
+import article from '../../storage/content/article';
 
 function Contact() {
   return (
     <Text title="Contact Simon">
       Brief contact details
+
+      <div>
+        {article}
+      </div>
     </Text>
   );
 }

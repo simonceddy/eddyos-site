d780c7dbf1857585cfcd377772338efcc8cf2865
Simon Eddy <simon@simoneddy.com.au>
2019-10-03 11:44:52 +1000

    readded rndHelper

---
+++ src/util/rndHelper.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-unused-vars */
+export default function rndHelper(setSize, setPos) {
+  /* const handleResize = () => {
+    console.log(setSize, setPos);
+  }; */
+
+  const onResizeStop = (e, dir, ref, delta) => {
+    // TODO set pos when resized from top or left
+    // console.log(delta);
+    setSize({
+      width: Number.parseInt(ref.style.width, 0),
+      height: Number.parseInt(ref.style.height, 0)
+    });
+  };
+
+  const onDragStop = (e, data) => {
+    // console.log(data);
+    setPos({ x: data.x, y: data.y });
+  };
+
+  return {
+    onResizeStop,
+    onDragStop
+  };
+}
